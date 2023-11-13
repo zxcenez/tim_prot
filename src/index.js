d20b7ea374d59f83c9a701a8bd79623013133c98
zxcenezi <vladislav.sidorov2008@gmail.com>
2023-11-13 19:46:59 +0200

    qwewqe

--- src/index.js
+++ src/index.js
@@ -5,3 +5,31 @@ $(document).ready(function () {
         $('body').toggleClass('fixed-page');
     });
 });
+
+// const smoothCoef = 0.05;
+// const smoothScroll = document.querySelector("body");
+// const smoothScrollBar = document.querySelector("body");
+
+// function onResize(e) {
+//     smoothScrollBar.style.height = 1000 + "px";
+// }
+
+// window.addEventListener("resize", onResize);
+// onResize();
+
+// let prevY = window.scrollY;
+// let curY = window.scrollY;
+// let y = window.scrollY;
+// let dy;
+
+// function loop(now) {
+//     curY = window.scrollY;
+//     dy = curY - prevY;
+//     y = Math.abs(dy) < 1 ? curY : y + dy * smoothCoef;
+//     prevY = y;
+//     smoothScroll.style.transform = `translate3d(0,${-y}px,0)`;
+
+//     requestAnimationFrame(loop);
+// }
+// requestAnimationFrame(loop);
+
